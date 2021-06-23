72787503d7a31bc7a116067629390910f78b23ea
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-24 00:42:47 +0300

    app ext initialisation

--- app/__init__.py
+++ app/__init__.py
@@ -1,32 +1,36 @@
-from flask import Flask
+from flask import Flask 
+from .config import config_options
+from flask_mail import Mail
+from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
-from config import config_options
 from flask_sqlalchemy import SQLAlchemy
-from  flask_migrate import Migrate, MigrateCommand
+from flask_uploads import IMAGES, UploadSet,configure_uploads
 
-
-bootstrap = Bootstrap()
 db = SQLAlchemy()
+mail = Mail()
+bootstap = Bootstrap()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+photos = UploadSet('photos',IMAGES)
+
+
 
 def create_app(config_name):
-
     app = Flask(__name__)
-
-    # Creating the app configurations
     app.config.from_object(config_options[config_name])
-
-    # Initializing flask extensions
-    bootstrap.init_app(app)
-    db.init_app(app)
-    
-
-    # Will add the views and forms
-     # Registering the blueprint
+    from .auth import auth as authentication_blueprint
     from .main import main as main_blueprint
+
+    app.register_blueprint(authentication_blueprint)
     app.register_blueprint(main_blueprint)
 
-    # setting config
-    # from .requests import configure_request
-    # configure_request(app)
+    login_manager.init_app(app)
+    db.init_app(app)
+    bootstap.init_app(app)
+    configure_uploads(app,photos)
+    mail.init_app(app)
+    
+
 
     return app
