d5c6dead11a2d25b7ca8a001afb8cd1f79f369cb
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-24 00:39:23 +0300

    adding my form

--- app/main/form.py
+++ app/main/form.py
@@ -1,9 +1,27 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField, SelectField, TextAreaField, SubmitField,DateField
 from wtforms.validators import Required
 
-class ReviewForm(FlaskForm):
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Write a brief bio about you.',validators = [Required()])
+    submit = SubmitField('Save')
 
-    title = StringField('Review title',validators=[Required()])
-    review = TextAreaField('Movie review', validators=[Required()])
-    submit = SubmitField('Submit')
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category', choices=[('Events','Events'),('Job','Job'),('Advertisement','Advertisement')],validators=[Required()])
+    # date_of_birth = DateField('Date of Birth', validators=[Required()])
+    hobbies= TextAreaField('Hobbies', validators=[Required()])
+    about=TextAreaField('About', validators=[Required()])
+    contact=TextAreaField('Contact', validators=[Required()])
+    twitter=TextAreaField('Twitter')
+    facebook=TextAreaField('Facebook')
+    instagram=TextAreaField('Instagram')
+    email=TextAreaField('Email')
+
+    post = TextAreaField('About Me', validators=[Required()])
+    submit = SubmitField('Your Post')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a comment',validators=[Required()])
+    submit = SubmitField('Comment')
+
