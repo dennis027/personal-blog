9efa743c8f5ad5a9135bda6b5d0a172f5af5ff3e
Dennis <dennis.kimani@student.moringaschool.com>
2021-06-24 10:36:44 +0300

    adding to heroku

--- app/config.py
+++ app/config.py
@@ -22,8 +22,7 @@ class ProdConfig(Config):
     """
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
-            "postgres://", "postgresql://")
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 class DevConfig(Config):
     """
     dev config child class
